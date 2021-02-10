efa924920385a131729f11d243e6fd21eeda49b0
Евгения <70897645+tortilla72@users.noreply.github.com>
2021-02-10 11:11:23 +0300

    Create api.php

---
+++ public_html/api.php
@@ -0,0 +1,14 @@
+<?
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+
+	$dbh = new PDO('mysql:host=localhost;dbname=cy42346_bd', 'cy42346_bd', 'cy42346_bd');
+	$stmt = $dbh->prepare("INSERT INTO `order`(`time`, name, email)
+		VALUES (NOW(), :name, :email)");
+	$stmt->bindParam(':name', $name);
+	$stmt->bindParam(':email', $email);
+	$stmt->execute();
+
+	echo '1';
+>
